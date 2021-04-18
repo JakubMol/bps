81bbb850613c264aa8097ae105b8054f02fda756
jmol <jakubandrzejmol@gmail.com>
2021-04-18 16:01:22 +0100

    feat: graph helpers added

---
+++ Source/pythonProject-20210320T135709Z-001/pythonProject/Graph.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl
+from matplotlib import pyplot
+import numpy as np
+
+
+def fromgrid(grid):
+    # make values from -5 to 5, for this example
+    zvals = np.random.rand(100, 100) * 10 - 5
+    grid,
+
+    # make a color map of fixed colors
+    cmap = mpl.colors.ListedColormap(['blue', 'black', 'red'])
+    bounds = [-6, -2, 2, 6]
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+    # tell imshow about color map so that only set colors are used
+    img = pyplot.imshow(zvals, interpolation='nearest',
+                        cmap=cmap, norm=norm)
+
+    # make a color bar
+    pyplot.colorbar(img, cmap=cmap,
+                    norm=norm, boundaries=bounds, ticks=[-5, 0, 5])
+
+    pyplot.show()
--- Source/pythonProject-20210320T135709Z-001/pythonProject/main.py
+++ Source/pythonProject-20210320T135709Z-001/pythonProject/main.py
@@ -1,9 +1,11 @@
 import Grid as Grid
+import Graph as Graph
 
 
 test_grid = Grid.Grid(20, 20)
 test_grid.addempty()
 test_grid.grids[0][0].state = 1
 test_grid.grids[0][1].state = 1
+Graph.fromgrid(test_grid.grids[0])
 test_grid.next()
 print(test_grid.tostring())
